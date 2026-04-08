2c9d520fb1d3a7250b317df19f3ef37d2968b771
jbleyel <jbleyel@gmx.net>
2026-04-08 20:40:23 +0200

    update github actions

--- CI/PEP8.sh
+++ CI/PEP8.sh
@@ -10,7 +10,7 @@ begin=$(date +"%s")
 
 echo ""
 echo "PEP8 double aggressive"
-autopep8 . -a -a -j 0 --recursive --select=E133,E401,E701,E70,E502,E251,E252,E20,E211,E22,E224,E241,E242,E27,E225,E226,E227,E228,E231,E261,E26,E301,E302,E303,E304,E305,E306,W291,W292,W293,W391 --in-place
+autopep8 . -a -a -j 0 --recursive --select=E133,E401,E701,E70,E502,E251,E252,E20,E211,E22,E224,E241,E242,E27,E225,E226,E227,E228,E231,E261,E301,E302,E303,E304,E305,E306,W291,W292,W293,W391 --in-place
 git add -u
 git add *
 git commit -m "PEP8"
--- src/PlutoTV/locale/updatepot.sh
+++ src/PlutoTV/locale/updatepot.sh
@@ -7,10 +7,8 @@
 #
 # Run this script from within the po folder.
 #
-# Author: Pr2 for OpenPLi Team
-# Version: 1.1
-#
-# Retrieve languages from Makefile.am LANGS variable for backward compatibility
+# Author: Pr2 for OpenPLi Team, jbleyel
+# Version: 2.0
 #
 localgsed="sed"
 findoptions=""
@@ -19,26 +17,26 @@ findoptions=""
 # Script only run with sed but on some distro normal sed is already sed so checking it.
 #
 sed --version 2> /dev/null | grep -q "GNU"
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
 	localgsed="sed"
 else
 	"$localgsed" --version | grep -q "GNU"
-	if [ $? -eq 0 ]; then
+	if [[ $? -eq 0 ]]; then
 		printf "GNU sed found: [%s]\n" $localgsed
 	fi
 fi
 
 which python
-if [ $? -eq 1 ]; then
+if [[ $? -eq 1 ]]; then
 	which python3
-	if [ $? -eq 1 ]; then
+	if [[ $? -eq 1 ]]; then
 		printf "python not found on this system, please install it first or ensure that it is in the PATH variable.\n"
 		exit 1
 	fi
 fi
 
 which xgettext
-if [ $? -eq 1 ]; then
+if [[ $? -eq 1 ]]; then
 	printf "xgettext not found on this system, please install it first or ensure that it is in the PATH variable.\n"
 	exit 1
 fi
@@ -59,24 +57,70 @@ fi
 # Arguments to generate the pot and po files are not retrieved from the Makefile.
 # So if parameters are changed in Makefile please report the same changes in this script.
 #
-printf "Creating temporary file PlutoTV-py.pot\n"
-find $findoptions .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d PlutoTV -s -o PlutoTV-py.pot {} \+
-$localgsed --in-place PlutoTV-py.pot --expression=s/CHARSET/UTF-8/
-printf "Creating temporary file PlutoTV-xml.pot\n"
-which python
-if [ $? -eq 0 ]; then
-	find $findoptions .. -name "setup.xml" -exec python xml2po.py {} \+ > PlutoTV-xml.pot
-else
-	find $findoptions .. -name "setup.xml" -exec python3 xml2po.py {} \+ > PlutoTV-xml.pot
-fi
-printf "Merging pot files to create: PlutoTV.pot\n"
-cat PlutoTV-py.pot PlutoTV-xml.pot | msguniq -s --no-wrap --no-location -o PlutoTV.pot -
+# Extract the plugin language domain from __init__.py or plugin.py
+PLUGIN_DOMAIN=$(grep -r "PluginLanguageDomain\s*=" .. | head -1 | $localgsed "s/.*PluginLanguageDomain\s*=\s*['\"]\\([^'\"]*\\)['\"].*/\\1/")
 
-languages=($(ls *.po | tr "\n" " " | $localgsed 's/.po//g'))
-for lang in "${languages[@]}" ; do
-	msgmerge --backup=none --no-wrap -s -U $lang.po PlutoTV.pot && touch $lang.po
-	msgattrib --no-wrap --no-obsolete --no-location $lang.po -o $lang.po
-done
+if [[ -z "$PLUGIN_DOMAIN" ]]; then
+	printf "Error: Could not find PluginLanguageDomain in __init__.py or plugin.py\n"
+	exit 1
+fi
+
+printf "Using plugin domain: %s\n" "$PLUGIN_DOMAIN"
+
+# Create backup of existing pot file if it exists
+if [[ -f "${PLUGIN_DOMAIN}.pot" ]]; then
+	cp "${PLUGIN_DOMAIN}.pot" "${PLUGIN_DOMAIN}.pot.bak"
+	printf "Backup created: %s.pot.bak\n" "$PLUGIN_DOMAIN"
+fi
+
+printf "Getting list of languages\n"
+languages=($(ls *.po 2>/dev/null | $localgsed 's/\.po$//g'))
+
+
+printf "Creating temporary file %s-py.pot\n" "$PLUGIN_DOMAIN"
+find $findoptions .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d "$PLUGIN_DOMAIN" --package-name="$PLUGIN_DOMAIN" -s -o ${PLUGIN_DOMAIN}-py.pot {} \+
+$localgsed --in-place ${PLUGIN_DOMAIN}-py.pot --expression=s/CHARSET/UTF-8/
+
+printf "Creating temporary file %s-xml.pot\n" "$PLUGIN_DOMAIN"
+which python
+if [[ $? -eq 0 ]]; then
+	find $findoptions .. -name "*.xml" -exec python xml2po.py {} \+ > ${PLUGIN_DOMAIN}-xml.pot
+else
+	find $findoptions .. -name "*.xml" -exec python3 xml2po.py {} \+ > ${PLUGIN_DOMAIN}-xml.pot
+fi
+printf "Merging pot files to create: %s.pot\n" "$PLUGIN_DOMAIN"
+cat ${PLUGIN_DOMAIN}-py.pot ${PLUGIN_DOMAIN}-xml.pot | msguniq -s --no-wrap -o ${PLUGIN_DOMAIN}.pot -
 printf "remove temp pot files\n"
-rm PlutoTV-py.pot PlutoTV-xml.pot
+rm ${PLUGIN_DOMAIN}-py.pot ${PLUGIN_DOMAIN}-xml.pot
+
+# Check if only POT-Creation-Date changed
+if [[ -f "${PLUGIN_DOMAIN}.pot.bak" ]]; then
+	# Remove POT-Creation-Date lines and compare
+	grep -v "^\"POT-Creation-Date:" "${PLUGIN_DOMAIN}.pot" > "${PLUGIN_DOMAIN}.pot.tmp1"
+	grep -v "^\"POT-Creation-Date:" "${PLUGIN_DOMAIN}.pot.bak" > "${PLUGIN_DOMAIN}.pot.tmp2"
+	
+	if diff -q "${PLUGIN_DOMAIN}.pot.tmp1" "${PLUGIN_DOMAIN}.pot.tmp2" > /dev/null; then
+		printf "No content changes detected (only POT-Creation-Date changed), restoring backup\n"
+		mv "${PLUGIN_DOMAIN}.pot.bak" "${PLUGIN_DOMAIN}.pot"
+	else
+		printf "Content changes detected, keeping new pot file\n"
+		rm "${PLUGIN_DOMAIN}.pot.bak"
+
+		for lang in "${languages[@]}" ; do
+			if [[ -f $lang.po ]]; then 
+				printf "Updating existing translation file %s.po\n" $lang
+				msgmerge --backup=none --no-wrap -s -U $lang.po ${PLUGIN_DOMAIN}.pot && touch $lang.po
+				msgattrib --no-wrap --no-obsolete $lang.po -o $lang.po
+			else
+				printf "New file created: %s, please add it to github before commit\n" $lang.po
+				msginit -l $lang.po -o $lang.po -i ${PLUGIN_DOMAIN}.pot --no-translator
+			fi
+		done
+
+	fi
+	
+	# Clean up temp files
+	rm -f "${PLUGIN_DOMAIN}.pot.tmp1" "${PLUGIN_DOMAIN}.pot.tmp2"
+fi
+
 printf "pot update from script finished!\n"
